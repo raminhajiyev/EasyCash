330d72b718529dc534ba671458983e88bb7a1d88
Ramin <ramin.haciyev.2014@gmail.com>
2024-01-14 17:40:07 +0100

    DTO Layers, Fluent Validation RuleFor

---
+++ BusinessLayer/ValidationRules/AppUserVR/AppUserRegisterValidator.cs
@@ -0,0 +1,28 @@
+﻿using DTOLayer.DTOs.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.AppUserVR
+{
+    public class AppUserRegisterValidator: AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+                RuleFor(x=>x.Name).MaximumLength(30).WithMessage("Max character for name is 30!");  
+                RuleFor(x=>x.Username).MinimumLength(5).WithMessage("Min character for username is 5!");  
+                RuleFor(x=>x.Surname).MaximumLength(30).WithMessage("Max character for surname is 30!");  
+                RuleFor(x=>x.Username).NotEmpty().WithMessage("Username can not be empty!");  
+                RuleFor(x=>x.Name).NotEmpty().WithMessage("Name can not be empty!");  
+                RuleFor(x=>x.Surname).NotEmpty().WithMessage("Surname can not be empty!");  
+                RuleFor(x=>x.Email).NotEmpty().WithMessage("Email can not be empty!");  
+                RuleFor(x=>x.Email).EmailAddress().WithMessage("Please use a valid email address!");  
+                RuleFor(x=>x.Password).NotEmpty().WithMessage("Password can not be empty!");  
+                RuleFor(x=>x.ConfirmPassword).NotEmpty().WithMessage("Confirm password can not be empty!");  
+                RuleFor(x=>x.ConfirmPassword).Equal(y=>y.Password).WithMessage("Passwords are not matching!");  
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-E8FO8AIP\\SQLEXPRESS; initial catalog=EasyCashDb; integrated security=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<AccountTransaction> AccountTransactions { get; set; }
+    }
+}
