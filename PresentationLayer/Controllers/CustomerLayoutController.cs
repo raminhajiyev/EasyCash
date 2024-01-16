e0b33c43b49c900a103d6fbc428dbe26b7bcd5e0
Ramin <ramin.haciyev.2014@gmail.com>
2024-01-16 21:32:06 +0100

    Updating authorized user's information

--- PresentationLayer/Controllers/CustomerLayoutController.cs
+++ PresentationLayer/Controllers/CustomerLayoutController.cs
@@ -2,7 +2,7 @@
 
 namespace PresentationLayer.Controllers
 {
-    public class MyProfileController : Controller
+    public class CustomerLayoutController : Controller
     {
         public IActionResult Index()
         {
---
+++ PresentationLayer/Controllers/MyAccountsController.cs
@@ -0,0 +1,56 @@
+﻿using DTOLayer.DTOs.AppUserDtos;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    [Authorize]
+    public class MyAccountsController : Controller
+    {
+    private readonly UserManager<AppUser> _userManager;
+
+        public MyAccountsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserEditDto appUserEditDto=  new AppUserEditDto();
+            appUserEditDto.Name=values.Name;
+            appUserEditDto.Surname=values.Surname;
+            appUserEditDto.PhoneNumber=values.PhoneNumber;
+            appUserEditDto.Email=values.Email;
+            appUserEditDto.City=values.City;    
+            appUserEditDto.District=values.District;
+            appUserEditDto.ImageUrl=values.ImageUrl;
+            return View(appUserEditDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserEditDto appUserEditDto)
+        {
+            if (appUserEditDto.Password==appUserEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = appUserEditDto.Name;
+                user.Surname = appUserEditDto.Surname;
+                user.PhoneNumber = appUserEditDto.PhoneNumber;
+                user.Email = appUserEditDto.Email;
+                user.City = appUserEditDto.City;
+                user.District = appUserEditDto.District;
+                user.ImageUrl = appUserEditDto.ImageUrl;
+                user.PasswordHash=_userManager.PasswordHasher.HashPassword(user,appUserEditDto.Password);
+                var result=await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+           return View();
+        }
+    }
+}
